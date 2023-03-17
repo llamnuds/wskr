9843e414e06da9eb1bd020b28d0210e615da2b6d
Shaun DUnmall <sdunmall@gmail.com>
2023-03-17 13:51:15 +0000

    Added a new feature in TO DO list.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ TO DO
 1) Add Open Source License to this code and a command to split it out on request.
 2) Add option to read computer list from AD.
 3) Make output file into two output files if -w=10
+4) Prevent WMI calls from applying changes or running programs.
 */
 
 package main
